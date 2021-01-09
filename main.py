b51386e6965798df6d2288966d6dfc4302de9e3b
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-09 19:52:53 +0700

    Revert "убрал бесконечный цикл"
    This reverts commit 08562ab01fe150ec2fc775471304db61a948d9be.

--- main.py
+++ main.py
@@ -13,34 +13,34 @@ def main():
     cfg = Config()
     bot = VkBot(cfg.TOKEN, cfg.VERSION)
 
-    for post in bot.get_all_posts(cfg.PUBLIC_ID):
+    while True:
+        for post in bot.get_all_posts(cfg.PUBLIC_ID):
 
-        # Проверка на существаование в базе
-        if service.is_exist(posts=db, post_id=post['id']):
-            logging.info(f"Пост {post['id']} определён")
-            continue
+            # Проверка на существаование в базе
+            if service.is_exist(posts=db, post_id=post['id']):
+                logging.info(f"Пост {post['id']} определён")
+                continue
 
-        post_data = {'id': post['id']}
-        
-        # Берём текст поста
-        if content := service.get_text(cfg=cfg, text=post['text']):
-            post_data.update({'text': content})
-            logging.info(f"Пост {post['id']} подходит")
-        else:
-            logging.info(f"Пост {post['id']} не подходит")
-            continue
+            post_data = {'id': post['id']}
+            
+            # Берём текст поста
+            if content := service.get_text(cfg=cfg, text=post['text']):
+                post_data.update({'text': content})
+                logging.info(f"Пост {post['id']} подходит")
+            else:
+                logging.info(f"Пост {post['id']} не подходит")
+                continue
 
-        # Добавлям фотки
-        post_data.update({"photos": list(service.get_photos(post['attachments']))})
-        logging.info(f"Фотографии поста {post['id']} добавлены")
+            # Добавлям фотки
+            post_data.update({"photos": service.get_photos(post['attachments'])})
+            logging.info(f"Фотографии поста {post['id']} добавлены")
 
-        # Добавляем в базу всю информацию о посте
-        db.append(post_data)
-        logging.info(f"Пост {post['id']} добавлен в базу данных")
+            # Добавляем в базу всю информацию о посте
+            db.append(post_data)
+            logging.info(f"Пост {post['id']} добавлен в базу данных")
 
-    # Делаем json-чик, чтобы поглядеть результат
-    service.make_json(db, 'database.json')
-    return db
+            # Делаем json-чик, чтобы поглядеть результат
+            service.make_json(db, 'database.json')
 
 
 if __name__ == '__main__':
